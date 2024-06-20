4f71b1a7c9b5422866ed73f9255d9c37e5ce2cce
huangguofu <guofu.gd@qq.com>
2024-06-20 18:54:38 +0800

    根据id修改管理员

--- cloud-shopping-manager-api/src/main/java/com/ithgf/controller/AdminController.java
+++ cloud-shopping-manager-api/src/main/java/com/ithgf/controller/AdminController.java
@@ -21,13 +21,27 @@ public class AdminController {
     @DubboReference
     private AdminService adminService;
 
-    // 添加管理员
+    /**
+     * 添加管理员
+     * @param admin 管理员对象
+     * @return 执行结果
+     */
     @PostMapping("/add")
     public BaseResult<Admin> add(@RequestBody Admin admin){
         adminService.add(admin);
         return BaseResult.success("添加管理员成功");
     }
-    // 更新管理员
+
+    /**
+     *  根据ID修改管理员
+     * @param admin 管理员对象
+     * @return 执行结果
+     */
+    @PutMapping("/update")
+    public BaseResult<Admin> update(@RequestBody Admin admin){
+        adminService.update(admin);
+        return BaseResult.success("修改管理员成功");
+    }
     // 删除管理员
     // 根据id查询管理员
     // 分页查询管理员
