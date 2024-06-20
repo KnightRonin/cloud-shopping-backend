3976b51b9d2004631225d3e90dc2d6477c3b1bd5
huangguofu <guofu.gd@qq.com>
2024-06-20 15:34:31 +0800

    API接口设置统一数据返回格式

---
+++ cloud-shopping-common/src/main/java/com/ithgf/result/BaseResult.java
@@ -0,0 +1,31 @@
+package com.ithgf.result;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @Title: BaseResult
+ * @Author ithgf
+ * @Package com.ithgf.result
+ * @Date 2024/6/20 15:18
+ * @description: API统一结果返回值
+ */
+
+@Data
+@AllArgsConstructor
+public class BaseResult<T> {
+    private Integer code;
+    private String message;
+    private T data;
+
+    // 构建成功结果
+    public static <T> BaseResult<T> success(){
+        return new BaseResult<T>(CodeEnum.SUCCESS.getCode(), CodeEnum.SUCCESS.getMessage(),null);
+    }
+    // 构建带有数据的成功结果
+    public static <T> BaseResult<T> success(T data){
+        return new BaseResult<T>(CodeEnum.SUCCESS.getCode(), CodeEnum.SUCCESS.getMessage(),data);
+    }
+
+
+}
---
+++ cloud-shopping-common/src/main/java/com/ithgf/result/CodeEnum.java
@@ -0,0 +1,22 @@
+package com.ithgf.result;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @Title: CodeEnum
+ * @Author ithgf
+ * @Package com.ithgf.result
+ * @Date 2024/6/20 15:21
+ * @description: 返回状态码枚举类
+ */
+
+@Getter
+@AllArgsConstructor
+public enum CodeEnum {
+    SUCCESS(200,"OK"),
+    ERROR(500,"系统异常");
+
+    private final Integer code;
+    private final String message;
+}
