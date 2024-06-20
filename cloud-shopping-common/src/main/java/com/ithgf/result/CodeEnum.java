3bf26795545646731c6e730b5220843dfcf6114b
huangguofu <guofu.gd@qq.com>
2024-06-20 15:45:43 +0800

    修改返回状态码枚举类

--- cloud-shopping-common/src/main/java/com/ithgf/result/CodeEnum.java
+++ cloud-shopping-common/src/main/java/com/ithgf/result/CodeEnum.java
@@ -15,8 +15,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum CodeEnum {
     SUCCESS(200,"OK"),
-    ERROR(500,"系统异常");
-
+    SYSTEM_ERROR(500,"系统异常"),
+    PARAMETER_ERROR(601,"参数异常");
     private final Integer code;
     private final String message;
+
 }
